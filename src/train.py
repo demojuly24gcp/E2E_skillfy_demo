5e2b3287b575dcfc690f6d6d0b3d1fc49a905f1d
demojuly24gcp <edurekajuly24gcp@gmail.com>
2025-08-02 17:52:37 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ df = pd.read_csv("/workspaces/codespaces-blank/E2E_skillfy_demo/data/Iris.csv")
 X = df.drop("Species", axis=1)
 y = df["Species"]
 
-model = LogisticRegression(max_iter=500)
+model = LogisticRegression(max_iter=600)
 model.fit(X, y)
 
 with open("/workspaces/codespaces-blank/E2E_skillfy_demo/model/model.pkl", "wb") as f:
