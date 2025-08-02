16f2300d53f57eeddd6601b52ff1a9e9f510859a
demojuly24gcp <edurekajuly24gcp@gmail.com>
2025-08-02 11:56:39 +0000

    changes

--- src/train.py
+++ src/train.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-df = pd.read_csv("data/iris.csv")
-X = df.drop("target", axis=1)
-y = df["target"]
+df = pd.read_csv("/workspaces/codespaces-blank/E2E_skillfy_demo/data/Iris.csv")
+X = df.drop("Species", axis=1)
+y = df["Species"]
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter=500)
 model.fit(X, y)
 
-with open("model/model.pkl", "wb") as f:
+with open("/workspaces/codespaces-blank/E2E_skillfy_demo/model/model.pkl", "wb") as f:
     pickle.dump(model, f)
